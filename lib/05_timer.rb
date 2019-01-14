693aa795c81f654754877fa556e2684e909dc754
Laspargus <maxime.speroni@gmail.com>
2019-01-14 20:28:54 +0100

    modification date

--- lib/05_timer.rb
+++ lib/05_timer.rb
@@ -1,23 +1,8 @@
-def time_string(a)
 
-    if a > 3559 then
-      hour = a/3600
-      r = a % 3600
-    elsif
-      hour = 0
-      r = a % 3600
-    end
-    
-    if r > 59 then 
-        min = r/60
-        sec = a % 60
-    elsif
-        min = 0
-        sec = r
-    end
-    
-    n = "%.2d" % hour, "%.2d" % min, "%.2d" % sec
-    
-    return "#{n[0]}:#{n[1]}:#{n[2]}"
-        
-    end
+def time_string(i)
+    #Time.at crée un objet date
+    #La méthode strftime permet de formater l'objet Time
+    return  Time.at(i).strftime "%H:%M:%S"
+      
+end
+
